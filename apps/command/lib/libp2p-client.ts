feba899d247bb6c938cc8b66d4317304d172db19
obregan <obregan@localhost>
2026-03-27 08:26:20 +0100

    fix(deps): replace @libp2p/noise with @chainsafe/libp2p-noise ^11.0.0

--- apps/command/lib/libp2p-client.ts
+++ apps/command/lib/libp2p-client.ts
@@ -1,7 +1,7 @@
 import { createLibp2p } from 'libp2p'
 import { webSockets } from '@libp2p/websockets'
 import { webRTC } from '@libp2p/webrtc'
-import { noise } from '@libp2p/noise'
+import { noise } from '@chainsafe/libp2p-noise'
 import { mplex } from '@libp2p/mplex'
 import { multiaddr } from '@multiformats/multiaddr'
 import { acquireTabLock } from './tab-lock'
